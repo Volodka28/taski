8513e8f279b44ffb4a2a01aca5599db04391527c
Volodka28 <buley28@gmail.com>
2023-06-15 18:47:47 +0300

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.2.224', '127.0.0.1', 'localhost', 'taskipracticum.hopto.org']
+ALLOWED_HOSTS = ['158.160.2.224', '127.0.0.1', 'localhost', 'taskipracticum.ddns.net']
 
 INSTALLED_APPS = [
     'api.apps.ApiConfig',
